a0dc15febedbbaed33ca51a834ddd3a088bfa2fb
philippschulte <philipp.schulte@ymail.com>
2018-01-02 11:41:00 -0800

    Fetch all service IDs if no IDs were provided in ids.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 'use strict';
 
+let ids = require('./ids');
 const sleep = require('./sleep');
 const config = require('./config');
-const services = require('./services');
 const fastly = require('fastly-promises');
 
 (async () => {
-  try {    
-    for (const id of services) {
+  try {
+    if (!ids.length) {
+      const services = await fastly(config.token).readServices();
+      ids = services.data.map(service => service.id);
+    }
+    
+    for (const id of ids) {
       const service = fastly(config.token, id);
       const versions = await service.readVersions();
       const active = versions.data.filter(version => version.active)[0];
       const backends = await service.readBackends(active.number);
-      const infected = backends.data.filter(config.infected);
+      const affected = backends.data.filter(config.affected);
       
-      if (!infected.length) continue;
+      if (!affected.length) continue;
 
       console.log(`Updating serivce: ${id}`);
       
       const clone = await service.cloneVersion(active.number);
-      await Promise.all(infected.map(backend => service.updateBackend(clone.data.number, backend.name, config.body)));
+      await Promise.all(affected.map(backend => service.updateBackend(clone.data.number, backend.name, config.body)));
       await service.activateVersion(clone.data.number);
       
       console.log(`Updated service: ${id}, version: ${clone.data.number}`);
